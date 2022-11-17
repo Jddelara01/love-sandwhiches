727bc6451b9ad0b1d20e819c099fadb50a902a7d
Jddelara01 <jddelara01@gmail.com>
2022-11-17 09:52:00 +0000

    Create get_sales_data function to get data string from user

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+"""Sales report for love sandwich."""
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -13,7 +14,19 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
 sales = SHEET.worksheet("sales")
-
 data = sales.get_all_values()
 
-print(data)
+
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10, 20, 30, 40, 50, 60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+
+get_sales_data()
